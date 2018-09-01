bf4c7339be8c8b0f2708802a674b40eccff4deae
juanurbanocordoba <42582212+juanurbanocordoba@users.noreply.github.com>
2018-08-31 22:19:42 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string palabra_con_minusculas;
+    cout<<"ingrese la frase pero cambie los espacios por guiones: ";
+    cin>>palabra_con_minusculas;
+    int ubicacion_de_la_letra=0, ascii_de_la_letra;
+    //este ciclo compara cada posicion del la cadena y mira si es minuscula y si detecta que lo es la baja a minuscula y la reemplaza
+    while(palabra_con_minusculas[ubicacion_de_la_letra]!=0){
+        ascii_de_la_letra=palabra_con_minusculas[ubicacion_de_la_letra];
+                if(ascii_de_la_letra>=97 && ascii_de_la_letra<=122){ascii_de_la_letra-=32;}
+                palabra_con_minusculas[ubicacion_de_la_letra]=ascii_de_la_letra;
+    ubicacion_de_la_letra+=1;
+    }
+    cout<<palabra_con_minusculas<<endl;
+    return 0;
+}
